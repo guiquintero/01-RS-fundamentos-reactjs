abddca59037d131f6812216cef6a7ec0550a9511
Guilherme Quintero Lorenzi <guiquintero02@gmail.com>
2024-02-22 11:23:15 -0300

    Atualizações de estilo e adição de funcionalidade de comentários

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,11 +1,13 @@
 import styles from "./Post.module.css"
+import { Comment } from "./Comment"
+import { Avatar } from "./Avatar"
 
 export function Post({ author, content }) {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/guiquintero.png" alt="" />
+        <Avatar src="http://github.com/guiquintero.png"/>
           <div className={styles.authorInfo}>
             <strong>Guilherme Quintero</strong>
             <span>Web Developer</span>
@@ -14,16 +16,29 @@ export function Post({ author, content }) {
 
         <time title="7 de fevereiro as 11:13" dataTime="2024-02-07"></time>
       </header>
-      <div className={styles.content}></div>
-      <p>Fala galera 🫰</p>
-      <p>Acabei de subir um projeto lá no meu gitHub</p>
+      <div className={styles.content}>
+        <p>Fala galera 🫰</p>
+        <p>Acabei de subir um projeto lá no meu gitHub</p>
 
-      <p>👉 <a href="">gui.design/slaoqmais</a></p>
+        <p>👉 <a href="">gui.design/slaoqmais</a></p>
 
-      <p>
-        <a href="">#novoprojeto</a> {''}
-        <a href="">#tmj</a>
-      </p>
+        <p>
+          <a href="">#novoprojeto</a> {''}
+          <a href="">#tmj</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe seu comentario" />
+        <footer>
+          <button type="submit">Comentar</button>
+        </footer>
+
+      </form>
+      <div className={styles.commentList}>
+        <Comment/>
+      </div>
     </article>
   )
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import {PencilLine} from '@phosphor-icons/react';
+import {Avatar} from './Avatar';
 
 import styles from "./Sidebar.module.css";
 
@@ -7,7 +8,7 @@ export function Sidebar(){
         <aside className={styles.sidebar}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
             <div className={styles.profile}>
-                <img src="https://github.com/guiquintero.png" alt="" />
+                <Avatar src="http://github.com/guiquintero.png"/>
                 <strong>Guilherme Quintero</strong>
                 <span>Front-End Developer</span>            
             </div>
